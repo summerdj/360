74f9bccfe3c1b4895c76df99fa595e129076991b
dj <895326770@qq.com>
2017-06-03 12:15:38 +0000

    vector ok

--- interview-master/vector/main.cc
+++ interview-master/vector/main.cc
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "qh_vector.h"
-
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
@@ -8,8 +8,84 @@ int main(int argc, char* argv[])
     //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
 
     qh::vector<int> num_vect;
-    num_vect.push_back(1);
+    std::cout<<"-----------------------test init function縮ize()縞apacity()------------------------"<< std::endl;
+	std::cout<<" vector<int> v : "<<std::endl;
+	std::cout<<"vector init size "<< num_vect.size() <<"   vector init capacity "<< num_vect.capacity()<< std::endl;
+	qh::vector<int> v(3,6);
+	std::cout<<" vector<int> v(3,6) : "<<std::endl;
+	std::cout<<"vector init size "<< v.size() <<"   vector init capacity "<< v.capacity()<< std::endl;
+	int *a = &v[0];
+	for(size_t  i = 0; i < v.size(); i++){
+		std::cout<< " content " << *(a + i) << " ";
+	}		
+	std::cout<<std::endl;
+	std::cout<<"-----------------------test [] operator------------------------"<< std::endl;
+	std::cout<<" use [] get items "<<std::endl;
+	for(size_t i = 0; i < v.size(); i++){
+		std::cout<< v[i] << " ";
+	}
+	std::cout<<std::endl;
+	std::cout<<"-----------------------test =  operator------------------------"<< std::endl;	
+	num_vect = v;
+	for(size_t i = 0; i< num_vect.size(); i++){
+		std::cout<< num_vect[i]<< " ";
+	}	
+	std::cout<<std::endl;
+	num_vect = num_vect;
+	std::cout<<"-----------------------test push_back() pop_back() ------------------------"<< std::endl;
+		std::cout<<" use [] get items "<<std::endl;
+	num_vect.push_back(1);
+	num_vect.push_back(2);
+	std::cout<<" size : "<< num_vect.size() << std::endl;
+	for(size_t i = 0; i< num_vect.size(); i++){
+    	 std::cout<< num_vect[i]<< " ";
+    }
+    std::cout<<std::endl;
+ 	num_vect.pop_back();
+	std::cout<<" size : "<< num_vect.size() << std::endl;
+    for(size_t i = 0; i< num_vect.size(); i++){
+    	std::cout<< num_vect[i]<< " ";
+	}    
+    std::cout<<std::endl;
+	std::cout<<"-----------------------test resize(size_t n)------------------------"<< std::endl;
+	num_vect.resize(2);
+	for(size_t i = 0; i< num_vect.size(); i++){
+        std::cout<< num_vect[i]<< " ";
+    }
+    std::cout<<std::endl;
+	std::cout<<"-----------------------test resize(size_t n, T &m)------------------------"<< std::endl;
+	num_vect.resize(8,8);
+	for(size_t i = 0; i< num_vect.size(); i++){
+        std::cout<< num_vect[i]<< " ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"-----------------------test reserve()------------------------"<< std::endl;
+	num_vect.reserve(20);
+	std::cout<<" size : "<< num_vect.size() <<"    capacity : "<< num_vect.capacity()<< std::endl;
+	num_vect.reserve(2);
+ 	std::cout<<" size : "<< num_vect.size() <<"    capacity : "<< num_vect.capacity()<< std::endl;
+	std::cout<<"-----------------------test clear() and  empty()------------------------"<< std::endl;
+	if(num_vect.empty()) 
+		std::cout<< " true,empty "<<std::endl;
+	else
+		std::cout<< " false,not empty "<<std::endl;
+	num_vect.clear();
+	if(num_vect.empty()) 
+		std::cout<< " true,empty "<<std::endl;
+    else
+        std::cout<< " false,not empty "<<std::endl;
 
+
+	//std::cout<<num_vect[0]<<std::endl;
+	//std::cout<<num_vect[1]<<std::endl;
+	//std::cout<<num_vect.size()<<std::endl;	
+ 
+/* 靠靠靠
+	int *x = new int[1];
+	std::cout<<x[0]<<std::endl;
+	x[1] = 2;
+	std::cout<<x[1]<<std::endl;
+*/
 #ifdef WIN32
     system("pause");
 #endif
--- interview-master/vector/qh_vector.h
+++ interview-master/vector/qh_vector.h
@@ -1,15 +1,28 @@
 #ifndef QIHOO_VECTOR_H_
 #define QIHOO_VECTOR_H_
-
+#include <iostream>
+#include <memory>
 namespace qh
 {
+/*
+ 	class testallocator : public std::allocator<int> {
+	public:
+		void construct(int *p, int val){
+			std::allocator<int>::construct(p, val);
+		}		
+		void destory(int *p){
+			std::allocator<int>::destroy(p);
+		}
+	};
+*/
     template<class T>
     class vector {
     public:
         //ctor
         vector()
-            : data_(NULL), size_(0)
+            : size_(0), capacity_(1)
         {
+			data_ = new T[capacity_];
         }
 
         explicit vector( size_t n, const T& value = T())
@@ -17,14 +30,26 @@ namespace qh
             data_ = new T[n];
             for (size_t i = 0; i < n; i++)
             {
-                data_[0] = value;
+                data_[i] = value;
             }
+			size_ = n;
+			capacity_ = n;
         }
 
-        vector<T>& operator=(const vector<T>& rhs)
+        const vector<T>& operator=(const vector<T>& rhs)
         {
-            data_ = rhs.data_;
-            size_ = rhs.size_;
+         	if (this != &rhs){
+				delete [] data_;
+				size_ = rhs.size();
+				capacity_ = rhs.capacity();
+				data_ = new T[size_];
+				for (size_t i = 0; i < size_; i++){
+					data_[i] = rhs.data_[i];
+				}							
+			}else{
+				std::cout<<" cannot set itself"<< std::endl;
+			}
+			return *this;
         }
 
         //dtor
@@ -38,27 +63,76 @@ namespace qh
 
         //get
         size_t size() const
-        {
+        {		
             return size_;
         }
 
         // set & get
-        T& operator[](size_t index);
-
+        T& operator[] (size_t index){
+			return *(data_+index);
+		}
         // set
-        void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize();
-        void reserve();
-        void clear();
-        void empty();
+        void push_back(const T& element){
+			if (size_ == capacity_){
+				reserve(2*capacity_);
+			}
+			data_[size_++] = element;	
+		}
+
+		void pop_back(){
+			size_--;
+		}
+		
+        void resize(const size_t newsize, const T &n){
+			if(newsize > size_){
+				if(newsize > capacity_){
+					int new_capacity = capacity_ * 2;
+					while(new_capacity < newsize)
+						new_capacity *= 2;
+					reserve(new_capacity);
+				}
+				for (size_t i = size_; i < newsize; i++){
+					std::allocator<int>().construct(data_ + i, n);
+				}
+				size_ = newsize;
+			}else{
+				for (size_t i = newsize; i < size_; i++){
+					std::allocator<int>().destroy(data_ + i);
+				}
+				size_ = newsize;
+			}
+		}
+		void resize(const size_t newsize){
+			resize(newsize, T());
+		}
+        void reserve(const size_t n){
+			if (n > capacity_){
+				T* temp = data_;
+				data_ = new T[n];
+				capacity_ = n;
+				for (size_t i = 0; i < size_; i++){
+					data_[i] = temp[i];
+				}  
+				delete temp;
+  			}
+		}
+        void clear(){
+			if(size_){
+				for(size_t i = 0; i< size_; i++)
+					std::allocator<int>().destroy(data_ + i);
+				size_ = 0;
+			}
+		}
+		size_t capacity() const {return capacity_;}
+        bool empty() const  {return size_ == 0;}
 
     private:
         T*      data_;
         size_t  size_;
+		size_t  capacity_; //vector容量
     };
 }
 
 #endif
 
-
+ 
